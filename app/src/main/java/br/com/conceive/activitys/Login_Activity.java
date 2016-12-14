23ff5b0b9ecb67245239c7314f5abf699dc097d1
Denis Viana <denisvcosta@outlook.com>
2016-12-13 21:27:30 -0300

    Commit

--- app/src/main/java/br/com/conceive/activitys/Login_Activity.java
+++ app/src/main/java/br/com/conceive/activitys/Login_Activity.java
@@ -27,11 +27,7 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
+
 
 import java.io.IOException;
 
